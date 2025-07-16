77d8df2c01805c82611bf1f5ab7fe658fa23b777
Hima Bindu Palukuri <112551922+himabindu79@users.noreply.github.com>
2025-07-16 07:09:01 +0000

    updated

---
+++ login-signup/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginSignup from './components/assets/login-signup/Login-Signup';
+
+function App() {
+  return (
+    <div>
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
